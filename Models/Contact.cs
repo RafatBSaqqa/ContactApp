17ddcd45abf06f3e732508769666265fbb6024a4
RafatBSaqqa <raafat.saqqa@gmail.com>
2024-04-06 04:26:41 +0300

    Contact CRUD Solved

---
+++ Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactCRUD.Models
+{
+    public class Contact
+    {
+       
+        [Required, Range(1, int.MaxValue, ErrorMessage = "The Id can't be smaller than 1.")]
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter a contact First name")]
+        public string FirstName { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Please enter a Last name.")]
+        public string LastName { get; set; } = string.Empty;
+        [EmailAddress(ErrorMessage = "Please give the Email. Thanks! :)")]
+        public string Email { get; set; } = string.Empty;
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+    }
+}
